818f7f44edb4ae4428bfa0545ad1460d9d0a6901
Maria <maria.alonso159@alu.ulpgc.es>
2023-11-22 14:51:03 +0000

    solved some problems

--- src/main/java/org/ulpgc/dacd/control/Main.java
+++ src/main/java/org/ulpgc/dacd/control/Main.java
@@ -13,8 +13,9 @@ public class Main {
             return;
         }
         String apiKey = args[0];
+        String dbPath = args[1];
         WeatherSupplier supplier = new OpenWeatherMapSupplier("https://api.openweathermap.org/data/2.5/forecast?", apiKey);
-        WeatherStore store = new SQLiteWeatherStore("jdbc:sqlite:src/main/java/jdbc/weather3.db");
+        WeatherStore store = new SQLiteWeatherStore("jdbc:sqlite:" + dbPath);
         WeatherController weatherControl = new WeatherController(days, supplier, store);
         Timer timer = new Timer();
         timer.scheduleAtFixedRate(new WeatherTask(weatherControl), 0, 6 * 60 * 60 * 1000);
--- src/main/java/org/ulpgc/dacd/control/OpenWeatherMapSupplier.java
+++ src/main/java/org/ulpgc/dacd/control/OpenWeatherMapSupplier.java
@@ -35,6 +35,7 @@ public class OpenWeatherMapSupplier implements WeatherSupplier {
     @Override
     public List<Weather> getWeathers(Location location, List<Instant> instants) {
         String url = buildUrl(location);
+        System.out.println(url);
         return instants.stream()
                 .map(instant -> {
                     try {
