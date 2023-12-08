5714a46e2eea9b862ce35a875cd4fe3703e34a02
Maria <maria.alonso159@alu.ulpgc.es>
2023-12-08 17:56:01 +0000

    weather-provider: improved and simplified periodic task

--- weather-provider/src/main/java/org/ulpgc/dacd/control/Main.java
+++ weather-provider/src/main/java/org/ulpgc/dacd/control/Main.java
@@ -4,7 +4,6 @@ import java.util.logging.Logger;
 
 public class Main {
     private static final Logger logger = Logger.getLogger(Main.class.getName());
-    private static final int days = 5;
 
     public static void main(String[] args) {
         if (args.length != 1) {
@@ -14,7 +13,8 @@ public class Main {
         String apiKey = args[0];
         WeatherSupplier supplier = new OpenWeatherMapSupplier(apiKey, "https://api.openweathermap.org/data/2.5/forecast?");
         JMSWeatherStore activeMQMessageSender = new JMSWeatherStore("tcp://localhost:61616");
-        WeatherController weatherController = new WeatherController(days, supplier, activeMQMessageSender);
+        WeatherController weatherController = new WeatherController(supplier, activeMQMessageSender);
+        weatherController.execute();
     }
 
 }
--- weather-provider/src/main/java/org/ulpgc/dacd/control/WeatherController.java
+++ weather-provider/src/main/java/org/ulpgc/dacd/control/WeatherController.java
@@ -4,76 +4,59 @@ import org.ulpgc.dacd.control.exceptions.StoreException;
 import org.ulpgc.dacd.model.Location;
 import org.ulpgc.dacd.model.Weather;
 
-import java.time.Instant;
-import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class WeatherController {
     private static final Logger logger = Logger.getLogger(WeatherController.class.getName());
     private final List<Location> locations;
-    private final int days;
     private final WeatherSupplier weatherSupplier;
     private final JMSWeatherStore jmsWeatherStore;
 
-    public WeatherController(int days, WeatherSupplier weatherSupplier, JMSWeatherStore jmsWeatherStore) {
-        this.days = days;
+    public WeatherController(WeatherSupplier weatherSupplier, JMSWeatherStore jmsWeatherStore) {
         this.locations = loadLocations();
         this.weatherSupplier = weatherSupplier;
         this.jmsWeatherStore = jmsWeatherStore;
-
-        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
-        scheduler.scheduleAtFixedRate(this::execute, 0, 1, TimeUnit.MINUTES);
     }
 
     public void execute() {
-        Instant currentTime = Instant.now();
-        List<Weather> weathers = unifyWeatherLists(currentTime);
-        sendEvents(weathers);
-        logger.info("Weather data update completed.");
+        Timer timer = new Timer();
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                List<Weather> weathers = unifyWeatherLists();
+                sendEvents(weathers);
+                logger.info("Weather data update completed.");
+            }
+        };
+        timer.schedule(task, 0, 1 * 60 * 1000);
     }
 
-    private List<Weather> unifyWeatherLists(Instant currentTime) {
+    private List<Weather> unifyWeatherLists() {
         List<Weather> weathers = new ArrayList<>();
-        List<Instant> forecastTimes = calculateForecastTimes(currentTime, days);
         for (Location location : locations) {
             try {
-                weathers.addAll(processLocation(location, forecastTimes));
+                weathers.addAll(weatherSupplier.getWeathers(location));
             } catch (Exception e) {
                 logger.log(Level.SEVERE, "Error processing location: " + e.getMessage(), e);
             }
         }
+        System.out.println(weathers);
         return weathers;
     }
 
-    private List<Weather> processLocation(Location location, List<Instant> forecastTimes) {
-        return weatherSupplier.getWeathers(location).stream()
-                .filter(weather -> forecastTimes.contains(weather.getPredictionTime()))
-                .collect(Collectors.toList());
-    }
-
     private void sendEvents(List<Weather> weathers) {
-        try{
+        try {
             for (Weather weather : weathers) {
                 jmsWeatherStore.save(weather);
             }
-        } catch (StoreException e){
+        } catch (StoreException e) {
             logger.log(Level.SEVERE, "Error sending events: " + e.getMessage(), e);
         }
-
-    }
-
-    private List<Instant> calculateForecastTimes(Instant currentTime, int days) {
-        return IntStream.range(0, days)
-                .mapToObj(i -> currentTime.plus(i + 1, ChronoUnit.DAYS).truncatedTo(ChronoUnit.DAYS).plus(12, ChronoUnit.HOURS))
-                .collect(Collectors.toList());
     }
 
     private static List<Location> loadLocations() {
@@ -89,4 +72,3 @@ public class WeatherController {
         return locations;
     }
 }
-
