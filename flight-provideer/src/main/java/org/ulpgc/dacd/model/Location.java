7062da78f933becbc4e20ccb3e3008b3945885d4
Maria <maria.alonso159@alu.ulpgc.es>
2024-01-05 14:51:46 +0000

    deleted flight-provider module

--- flight-provideer/src/main/java/org/ulpgc/dacd/model/Location.java
+++
@@ -1,9 +0,0 @@
-package org.ulpgc.dacd.model;
-
-public record Location(String name, double lat, double lon) {
-
-    @Override
-    public String toString() {
-        return name() + "(" + lat() + ", " + lon() + ")";
-    }
-}
--- flight-provider/src/main/java/org/ulpgc/dacd/control/exceptions/StoreException.java
+++
@@ -1,7 +0,0 @@
-package org.ulpgc.dacd.control.exceptions;
-
-public class StoreException extends Exception {
-    public StoreException(String message) {
-        super(message);
-    }
-}
