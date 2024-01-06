c406c6b4d286fbc317381c887543e83ac5df6754
Maria <maria.alonso159@alu.ulpgc.es>
2024-01-06 14:02:52 +0000

    weather-responsive-hotel-offer-advisor: added clearTables() method

--- bussiness/src/main/java/org/ulpgc/dacd/control/Main.java
+++ bussiness/src/main/java/org/ulpgc/dacd/control/Main.java
@@ -1,6 +1,7 @@
 package org.ulpgc.dacd.control;
 
 import org.ulpgc.dacd.control.exceptions.EventReceiverException;
+import org.ulpgc.dacd.control.exceptions.DataMartStoreException;
 import org.ulpgc.dacd.view.HotelRecommendationAPI;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -25,19 +26,29 @@ public class Main {
             logger.severe("Insufficient arguments. Usage: Main <baseDirectory> <topic1> <topic2> ...");
             return;
         }
-        String dataLakeDirectory = args[0];
-        String dataMartDirectory = args[1];
-        List<String> topicNames = Arrays.asList(args[2], args[3]);
-        DataLakeAccessor dataLakeAccessor = new DataLakeAccessor(dataLakeDirectory);
-        DataMartStore dataMartStore = new SqLiteDataMartStore(dataMartDirectory);
-        Map<String, Handler> handlerMap = initializeHandlers(dataMartStore, topicNames);
-        TopicSubscriber topicSubscriber = new TopicSubscriber(brokerUrl, topicNames, "clientId", handlerMap);
-        DataMartBuilder dataMartBuilder = new DataMartBuilder(dataLakeAccessor, handlerMap.get(topicNames.get(0)), handlerMap.get(topicNames.get(1)));
-        dataMartBuilder.buildDataMart();
-        DataMartConsultant dataMartConsultant = new DataMartConsultant(dataMartDirectory);
-        HotelRecommendationAPI hotelAPI = new HotelRecommendationAPI(dataMartConsultant);
-        hotelAPI.init();
-        startTopicSubscriber(topicSubscriber);
+
+        try {
+            String dataLakeDirectory = args[0];
+            String dataMartDirectory = args[1];
+            List<String> topicNames = Arrays.asList(args[2], args[3]);
+            DataLakeAccessor dataLakeAccessor = new DataLakeAccessor(dataLakeDirectory);
+            DataMartStore dataMartStore = new SqLiteDataMartStore(dataMartDirectory);
+            Map<String, Handler> handlerMap = initializeHandlers(dataMartStore, topicNames);
+            TopicSubscriber topicSubscriber = new TopicSubscriber(brokerUrl, topicNames, "clientId", handlerMap);
+            DataMartBuilder dataMartBuilder = new DataMartBuilder(dataLakeAccessor, handlerMap.get(topicNames.get(0)), handlerMap.get(topicNames.get(1)));
+            dataMartBuilder.buildDataMart();
+            DataMartConsultant dataMartConsultant = new DataMartConsultant(dataMartDirectory);
+            HotelRecommendationAPI hotelAPI = new HotelRecommendationAPI(dataMartConsultant);
+            hotelAPI.init();
+            startTopicSubscriber(topicSubscriber);
+        } finally {
+
+            Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+                logger.info("Cerrando la aplicación. Limpiando datos de las tablas...");
+                clearTables(args);
+                logger.info("Datos de las tablas limpiados.");
+            }));
+        }
     }
 
     private Map<String, Handler> initializeHandlers(DataMartStore dataMartStore, List<String> topicNames) {
@@ -59,5 +70,15 @@ public class Main {
             }
         });
         executorService.shutdown();
+    }
+
+    private void clearTables(String[] args) {
+        try {
+            String dataMartDirectory = args[1];
+            DataMartStore dataMartStore = new SqLiteDataMartStore(dataMartDirectory);
+            ((SqLiteDataMartStore) dataMartStore).clearTables();
+        } catch (DataMartStoreException e) {
+            logger.log(Level.SEVERE, "Error clearing tables", e);
+        }
     }
 }
